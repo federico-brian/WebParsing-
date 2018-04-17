f0ed7d7395791ab7f96680cedb5373b7c5d3dc0b
FedericoBrian <you@example.com>
2018-04-17 19:37:02 +0200

    urllib.request

--- DreamSub/src/main.py
+++ DreamSub/src/main.py
@@ -1,8 +1,11 @@
 import bs4 as bs 
-import urllib2 as ur 
+import urllib.request as ur 
 
 #Developer: Fedrerico Brian 
 #Mail federigus04@gmail.com
 #Data 17/04/2018
 
-
+def main():
+	pass
+if __name__ == '__main__':
+	main()
